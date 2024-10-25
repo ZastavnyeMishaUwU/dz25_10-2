32f2d54f01430e719ee4a743024979956991e3a3
xlibnui_makix <103821356+ZastavnyeMishaUwU@users.noreply.github.com>
2024-10-25 20:19:48 +0300

    Add project files.

---
+++ dz25_10-2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace dz25_10_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введіть кількість реалізованих проектів: ");
+            int project = int.Parse(Console.ReadLine());
+            Console.Write("Введіть рівень якості : ");
+            double vidcotok = double.Parse(Console.ReadLine());
+            string result = (project > 10 && vidcotok >= 80) ? "Бонус" : "Немає бонусу";
+            Console.WriteLine(result);
+        }
+    }
+}
